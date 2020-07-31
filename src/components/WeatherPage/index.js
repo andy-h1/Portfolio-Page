2d124373a560bb1d8c62c38432474568d7a43a94
andy-h1 <a.huynh2612@gmail.com>
2020-07-31 22:28:26 +0100

    feat(weather): updating weather input API call

--- src/components/WeatherPage/index.js
+++ src/components/WeatherPage/index.js
@@ -3,7 +3,14 @@ import axios from 'axios';
 
 export const WeatherPage = () => {
         const [weatherData, setWeatherData] = useState();
-        const [city, setCity] = useState();
+        const [city, setCity] = useState('Croydon');
+        const [country, setCountry] = useState('GB');
+        const handleCityChange = event => {
+                setCity(event.target.value);
+        };
+        const handleCountryChange = event => {
+                setCountry(event.target.value);
+        };
 
         console.log({ test: weatherData });
 
@@ -11,14 +18,14 @@ export const WeatherPage = () => {
                 async function fetchData() {
                         // move API key to env vars
                         const { data } = await axios.get(
-                                'https://api.openweathermap.org/data/2.5/weather?q=Croydon,uk&units=metric&APPID=7fe63c086a813a868027767b80769bcf'
+                                `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&APPID=7fe63c086a813a868027767b80769bcf`
                         );
                         const { weather, main, sys, name } = data;
                         setWeatherData({ weather, main, sys, name });
                 }
 
                 fetchData();
-        }, []);
+        }, [city, country]);
 
         return (
                 <div>
@@ -40,20 +47,24 @@ export const WeatherPage = () => {
                                         </p>
                                         <p>Change location:</p>
                                         <form>
-                                                <label>
-                                                        City: <input type="text" />
-                                                </label>
-                                                <input type="submit" />
+                                                <input
+                                                        type="text"
+                                                        placeholder="Enter city"
+                                                        name="city"
+                                                        value={city}
+                                                        onChange={handleCityChange}
+                                                />
+                                                <input
+                                                        type="text"
+                                                        placeholder="Enter country"
+                                                        name="country"
+                                                        value={country}
+                                                        onChange={handleCountryChange}
+                                                />
+                                                <button type="submit">Get Weather</button>
                                         </form>
                                 </div>
                         )}
                 </div>
         );
 };
-
-// What do I want to do next with this Weather Page
-
-// Display visual representation of the weather
-// - use weather.icon to display visuals
-
-// Add input form to change location of the weather data
