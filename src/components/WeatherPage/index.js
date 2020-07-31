e5dd088237e9d71585da74f9666d43ecd9f04d58
andy-h1 <a.huynh2612@gmail.com>
2020-07-31 13:17:47 +0100

    feat(weatherimage): display image based on current temperature

--- src/components/WeatherPage/index.js
+++ src/components/WeatherPage/index.js
@@ -4,43 +4,47 @@ import axios from 'axios';
 export const WeatherPage = () => {
         const [weatherData, setWeatherData] = useState();
 
-        console.log({ test: weatherData?.name });
-
+        console.log({ test: weatherData });
         useEffect(() => {
                 async function fetchData() {
                         // move API key to env vars
                         const { data } = await axios.get(
-                                'https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=7fe63c086a813a868027767b80769bcf'
+                                'https://api.openweathermap.org/data/2.5/weather?q=Croydon,uk&units=metric&APPID=7fe63c086a813a868027767b80769bcf'
                         );
-
                         const { weather, main, sys, name } = data;
-
                         setWeatherData({ weather, main, sys, name });
                 }
 
                 fetchData();
         }, []);
 
-        // what parts of the data do you actually need?
-
         return (
                 <div>
-                        <p>Weather!!!</p>
                         {weatherData && (
                                 <div>
-                                        <h1>Currently in {weatherData.name}</h1>
+                                        <h1>Current Weather</h1>
+                                        <img
+                                                src={`http://openweathermap.org/img/wn/${
+                                                        weatherData.weather[0].icon
+                                                }@2x.png`}
+                                                alt={weatherData.weather[0].main}
+                                        />
+                                        <p>
+                                                Location: {weatherData.name}, {weatherData.sys.country}
+                                        </p>
                                         <p>
                                                 The weather is currently {weatherData.main.temp} with{' '}
                                                 {weatherData.weather[0].description}{' '}
                                         </p>
                                 </div>
                         )}
-
-                        {/* {weatherData && <WeatherTracker details={Object.entries(weatherData)} />} */}
                 </div>
         );
 };
-// How to build a Weather App
-// It should display - current location, current temperature, weather condition, humidity
-// How do I find my current location?
-//
+
+// What do I want to do next with this Weather Page
+
+// Display visual representation of the weather
+// - use weather.icon to display visuals
+
+// Add input form to change location of the weather data
