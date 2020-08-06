cf0a08d5c4bec43184ece867357941551211d995
andy-h1 <a.huynh2612@gmail.com>
2020-08-06 11:47:07 +0100

    feat(weather): fixed the weather and form component

--- src/components/WeatherPage/index.js
+++ src/components/WeatherPage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import * as S from './styles';
+import { WeatherTracker } from '../WeatherTracker';
 
 export const WeatherPage = () => {
   const [weatherData, setWeatherData] = useState();
@@ -8,90 +9,82 @@ export const WeatherPage = () => {
   const [country, setCountry] = useState('GB');
   const [unit, setUnit] = useState('metric');
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
   const handleCityChange = (event) => {
     setCity(event.target.value);
   };
+
   const handleCountryChange = (event) => {
     setCountry(event.target.value);
   };
+
   const handleUnitChange = (event) => {
     setUnit(event.target.value);
   };
-  // eslint-disable-next-line no-console
-  console.log({ test: weatherData });
-  // eslint-disable-next-line no-console
-  console.log(unit);
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=${unit}&APPID=${API_KEY}`
+    );
+    const { weather, main, sys, name } = data;
+    setWeatherData({ weather, main, sys, name });
+  };
 
   useEffect(() => {
-    async function fetchData() {
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=${unit}&APPID=${API_KEY}`
-      );
-      const { weather, main, sys, name } = data;
-      setWeatherData({ weather, main, sys, name });
+    // only fetch data if no weatherData
+    if (!weatherData) {
+      fetchData();
     }
-
-    fetchData();
-  }, [API_KEY, city, country, unit]);
+  }, [fetchData, weatherData]);
 
   return (
     <>
       <S.Title>Current Weather</S.Title>
-      {weatherData && (
-        <S.Wrapper>
-          <S.Image
-            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
-            alt={weatherData.weather[0].main}
-          />
-          <p>
-            Location: {weatherData.name}, {weatherData.sys.country}
-          </p>
-          <p>
-            The weather is currently {Math.round(weatherData.main.temp)}° with{' '}
-            {weatherData.weather[0].description}{' '}
-          </p>
-          <p>Change location:</p>
-          <form>
-            <label htmlFor="cityInput">
-              <input
-                type="text"
-                placeholder="Enter city"
-                name="city"
-                onChange={handleCityChange}
-              />
-            </label>
-            <label htmlFor="countryInput">
-              <input
-                type="text"
-                placeholder="Enter country"
-                name="country"
-                onChange={handleCountryChange}
-              />
-            </label>
-            <label htmlFor="unitInput">
-              <input
-                type="radio"
-                name="units"
-                checked={unit === 'metric'}
-                value="metric"
-                onChange={handleUnitChange}
-              />
-              Celsius
-            </label>
-            <label htmlFor="unitInput">
-              <input
-                type="radio"
-                name="units"
-                checked={unit === 'imperial'}
-                value="imperial"
-                onChange={handleUnitChange}
-              />
-              Fahrenheit
-            </label>
-            {/* <button type="submit">Get Weather</button> */}
-          </form>
-        </S.Wrapper>
-      )}
+      {weatherData && <WeatherTracker details={weatherData} />}
+      <S.Wrapper>
+        <p>Change location:</p>
+        <form>
+          <label htmlFor="cityInput">
+            <input
+              type="text"
+              placeholder="Enter city"
+              name="city"
+              onChange={handleCityChange}
+            />
+          </label>
+          <label htmlFor="countryInput">
+            <input
+              type="text"
+              placeholder="Enter country"
+              name="country"
+              onChange={handleCountryChange}
+            />
+          </label>
+          <label htmlFor="unitInput">
+            <input
+              type="radio"
+              name="units"
+              checked={unit === 'metric'}
+              value="metric"
+              onChange={handleUnitChange}
+            />
+            Celsius
+          </label>
+          <label htmlFor="unitInput">
+            <input
+              type="radio"
+              name="units"
+              checked={unit === 'imperial'}
+              value="imperial"
+              onChange={handleUnitChange}
+            />
+            Fahrenheit
+          </label>
+          <button type="button" onClick={fetchData}>
+            Get Weather
+          </button>
+        </form>
+      </S.Wrapper>
     </>
   );
 };
--- src/components/WeatherTracker/index.js
+++ src/components/WeatherTracker/index.js
@@ -1,5 +1,22 @@
-// import React from 'react';
+import React from 'react';
 
-// export const WeatherTracker = ({}) => (
-
-// )
+export const WeatherTracker = ({ details }) => {
+  // eslint-disable-next-line no-console
+  const { weather, name, sys, main } = details;
+  return (
+    <div>
+      {console.log({ details })}
+      <img
+        src={`http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`}
+        alt={weather[0].main}
+      />
+      <p>
+        Location: {name}, {sys.country}
+      </p>
+      <p>
+        The weather is currently {Math.round(main.temp)}° with{' '}
+        {weather[0].description}{' '}
+      </p>
+    </div>
+  );
+};
